e2112e77b6ea4ef433cf49b04768304c15b4a4f6
liushuai <873895752@qq.com>
2022-04-06 15:12:25 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, url_for
+from flask import Flask, escape, url_for, render_template
 app = Flask(__name__)
 #接下来，我们要注册一个处理函数，这个函数是处理某个请求的处理函数，
 # Flask 官方把它叫做视图函数（view funciton），你可以理解为“请求处理函数”。
@@ -26,3 +26,19 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for', num=2)) #输出/test?num=2
     return 'Test page'
+name = 'liushaui'
+movies = [
+{'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=mobies)
